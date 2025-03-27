5e381136cb597e82d5ae782300ba2a3f04b4677b
David Brown <dgb_online@icloud.com>
2025-03-27 18:39:55 -0400

    Add Blog POsts, #2

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 
 from django.urls import path
-from . import views
+from .views import PostListView, PostDetailView
 
 
 urlpatterns = [
-    path('', views.home, name = 'home'),
+    path('', PostListView.as_view(), name = 'post-list'),
+    path('post_detail/<int:pk>', PostDetailView.as_view(), name = 'post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 
-def home(request):
-    return render(request, 'home.html', {})
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
