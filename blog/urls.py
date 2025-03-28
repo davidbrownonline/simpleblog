b2e8d39edca1da59e1552e4931e4f3f6c545b874
David Brown <dgb_online@icloud.com>
2025-03-28 06:35:46 -0400

    Add Post #4

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name = 'post-list'),
     path('post_detail/<int:pk>', PostDetailView.as_view(), name = 'post-detail'),
+    path('post_create/', PostCreateView.as_view(), name = 'post-create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -11,3 +11,10 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_create.html'
+    fields = '__all__'
+
