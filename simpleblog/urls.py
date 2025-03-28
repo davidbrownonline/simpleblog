d2be789750f16154a9cf287c2c44959753c724d5
David Brown <dgb_online@icloud.com>
2025-03-28 15:37:41 -0400

    Authentication #9

--- simpleblog/urls.py
+++ simpleblog/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('members/', include('django.contrib.auth.urls')), # will look for builtin urls first
+    path('members/', include('members.urls')), # otherwise looks at members/urls.py
 ]
